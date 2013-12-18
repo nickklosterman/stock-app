27dc6d5d00d8b8f297f65cd564baaae88b2cbf36
nick klosterman <nick_klosterman@yahoo.com>
2013-12-18 12:00:48 -0500

    added sparklines and pulling rank/date info for details

--- server/routes/index.js
+++ server/routes/index.js
@@ -43,12 +43,33 @@ exports.index = function index(req, res) {
 
 };
 
+function getTickersRanking(ticker,callback) {
+console.log('get date and ranking info from db');
+    var queryResult={};
+    queryResult.data=[]; //for handlebars, to use #each, I need to package into a named array.
+    var sql = "SELECT id,date,rank from BC20 where StockTicker like \""+ticker+"\" order by date Asc";
+    db.all(sql,function(err,rows){
+	rows.forEach(function (row) {
+	    console.log(row);
+	    queryResult.data.push(row);
+	});
+	closeDb();
+	callback( queryResult);
+    });
+
+};
+
 exports.detail = function detail(req, res) {
   // TODO: get stock data for symbol.
-  var data = {
-    symbol: 'aapl'
-  , rank: '2'
-  };
-  
-  return res.render('detail', { stock: data });
+createDb();
+    console.log(req.params.id)
+  // var data = {
+  //   symbol: 'aapl'
+  // , rank: '2'
+  // };
+getTickersRanking(req.params.id, function(data) {
+    data.ticker = req.params.id;
+  return res.render('detail', data);
+});
+//
 };
