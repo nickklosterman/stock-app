52965dd92bd49a45ea3054d4201b8e893a50d63f
nick klosterman <nick_klosterman@yahoo.com>
2013-12-17 23:19:05 -0500

    cleaned up my 'learning' code

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,12 +9,10 @@ db = new sqlite3.Database('IBDTestDatabaseBC20.sqlite');
 
 function getDistinctTickers(callback) {
     console.log('get distinct tickers');
-    //var queryResult=[];
     var queryResult={};
     queryResult.data=[]; //for handlebars, to use #each, I need to package into a named array.
     var sql = "SELECT distinct(stockticker) from BC20 order by stockticker Asc";
     db.all(sql,function(err,rows){
-	//	queryResult.data = rows;
 	rows.forEach(function (row) {
 	    console.log(row);
 	    queryResult.data.push(row);
@@ -22,7 +20,7 @@ function getDistinctTickers(callback) {
 	closeDb();
 	callback( queryResult);
     });
-    
+  
 
     //it's async so we return inside the callback.
 };
@@ -34,22 +32,15 @@ function closeDb() {
 
 exports.index = function index(req, res) {
     createDb();
-    // var queryResult = getDistinctTickers();
-    // if (queryResult && queryResult.length > 0 ) {
-    // 	console.log(queryResult.length);
-    // 	return res.render('index', queryResult);
-    // 	}
 
 //need to use a callback since this shit be all async yo
     getDistinctTickers(function(data) {
 	console.log("now go render");
 	console.log(data)
-//	data = { name:"bob", age:32};
 	return	res.render('index',data);
 
     });    
-    //  console.log(queryResult);
-    //    return res.render('index');
+
 };
 
 exports.detail = function detail(req, res) {
