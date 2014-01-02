b65128370a14477c50e4912f15a779642f4b67b8
Matt Hernandez <matt@modulus.io>
2014-01-01 21:28:51 -0500

    Documentation.
    Also removed the router console logs.

--- public/js/app.js
+++ public/js/app.js
@@ -24,12 +24,17 @@ window.StockApp.Collections = {};
   // =====
   //
 
+  //
+  // Includes the details for a specific stock. Renders the details in a list
+  //    and shows a line charts for specific fields.
+  //
   StockApp.Views.Detail = Backbone.View.extend({
     el: '.detail',
     template: _.template($('#detail').html()),
     initialize: function(options) {
       this.ticker = options.ticker;
       this.model = new StockApp.Models.Stock();
+      // Set the model's url to get the data for the correct ticker.
       this.model.url = function() {
         return this.rootUrl + options.ticker;
       };
@@ -37,11 +42,13 @@ window.StockApp.Collections = {};
       this.model.fetch({ success: _.bind(this.render, this) });
     },
     render: function() {
+      // Render the view with both the model data and the ticker name.
       this.$el.html(this.template({
         ticker: this.ticker,
         data: this.model.toJSON()
       }));
 
+      // Configure each of the line charts.
       this.$el.find('.linechart').sparkline('html', {
         chartRangeMin: 1,
         chartRangeMax: 20
@@ -57,6 +64,10 @@ window.StockApp.Collections = {};
     }
   });
 
+  //
+  // Shows every available stock. Includes a child detail view to show details
+  //    for a stock when clicked.
+  //
   StockApp.Views.Index = Backbone.View.extend({
     el: '.list',
     template: _.template($('#index').html()),
@@ -86,8 +97,9 @@ window.StockApp.Collections = {};
       ':ticker': 'detail'
     },
     index: function() {
-      console.log('router::view::index');
-
+      //
+      // Only instantiate the index view if it needs to it.
+      //
       if (this.indexView) {
         this.indexView.hideDetail();
       } else {
@@ -97,8 +109,6 @@ window.StockApp.Collections = {};
       }
     },
     detail: function(ticker) {
-      console.log('router::view::detail');
-
       //
       // Ensure that links directly to the detail for a ticker correctly
       //    instantiate the index view. This will correctly save the state
