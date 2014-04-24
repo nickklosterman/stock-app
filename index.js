23a5c40ff5edccf6d1e640ffe1d303c8a42e3383
nickklosterman <nick_klosterman@yahoo.com>
2014-04-24 13:08:51 -0400

    allow passing in on command line the database to use

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var express = require('express')
-  , routes  = require('./server/routes')
+  //, routes  = require('./server/routes')(app,process.argv[2] || 'IBDTestDatabaseBC20.sqlite')
   , path    = require('path')
   , app     = express();
+var routes  = require('./server/routes')(app,process.argv[2] || 'IBDTestDatabaseBC20.sqlite');
+
+//http://stackoverflow.com/questions/4351521/how-to-pass-command-line-arguments-to-node-js http://nodejs.org/docs/latest/api/process.html#process_process_argv
+if ( false ) {
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
+});
+}
 
 app.set('port', process.env.PORT || 3000);
 app.set('isprod', process.env.NODE_ENV === 'production');
@@ -25,9 +33,8 @@ if ('development' === app.get('env')) {
 // Routes
 // ======
 //
-
-app.get('/', routes.index);
-app.get('/api/stocks/:id?', routes.stocks);
+//app.get('/', routes.index);
+//app.get('/api/stocks/:id?', routes.stocks);
 
 // app.get('/dates/:date?', routes.dates);
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 var sqlite3 = require('sqlite3')
   , util    = require('util');
+var sqlite_database="";
+
 
 function compute_percentage_growth(rows) {
   //check since sometimes some records missing from database
@@ -24,9 +26,10 @@ exports.index = function(req, res) {
 // Get data for all dates or specific date data if specified.
 //
 exports.dates = function(req, res) {
-  //var db = new sqlite3.Database('IBDTestDatabaseBC20.sqlite');
-var db = new sqlite3.Database('IBDdatabase.sqlite');
-
+//  var db = new sqlite3.Database('IBDTestDatabaseBC20.sqlite');
+//var db = new sqlite3.Database('IBDdatabase.sqlite');
+  //  console.log("exports.dates using:"+sqlite_database);
+   var db = new sqlite3.Database(sqlite_database);
   if (req.params.date) {
     db.all(
       util.format('SELECT rank,stockticker from BC20 where Date="%s" order by Rank Asc', req.params.date),
@@ -49,12 +52,14 @@ var db = new sqlite3.Database('IBDdatabase.sqlite');
 //
 exports.stocks = function(req, res) {
   //var db = new sqlite3.Database('IBDTestDatabaseBC20.sqlite');
-var db = new sqlite3.Database('IBDdatabase.sqlite');
-
+//var db = new sqlite3.Database('IBDdatabase.sqlite');
+//    console.log("exports.stocks using:"+sqlite_database);
+   var db = new sqlite3.Database(sqlite_database);
+    var table = "BC20";
   if (req.params.id) {
     db.all(
 //      util.format('SELECT date,open,rank FROM BC20_%s_Master ORDER BY date ASC', req.params.id)
-      util.format('SELECT date,open,rank FROM BC20_%s_Master WHERE date >= ( SELECT min(date) from BC20_%s_Master WHERE rank > 0 and  date >= "2013-01-01" and date < "2013-12-31"  ) ORDER BY date ASC', req.params.id, req.params.id),
+      util.format('SELECT date,open,rank FROM '+table+'_%s_Master WHERE date >= ( SELECT min(date) from '+table+'_%s_Master WHERE rank > 0 and  date >= "2013-01-01" and date < "2013-12-31"  ) ORDER BY date ASC', req.params.id, req.params.id),
       //util.format('SELECT date,open,rank FROM BC20_%s_Master WHERE date > "2013-05-01" ', req.params.id), //WORKS
 	//util.format('SELECT date,open,rank FROM BC20_%s_Master WHERE date > "%s" ', req.params.id,"2013-04-15"), //WORKS
 
@@ -75,4 +80,13 @@ var db = new sqlite3.Database('IBDdatabase.sqlite');
         res.json(rows);
       });
   }
+};
+
+//http://stackoverflow.com/questions/13151693/passing-arguments-to-require-when-loading-module
+//https://www.google.com/search?q=exports.module
+module.exports = function(app,db) { 
+sqlite_database=db;
+console.log(sqlite_database+",db: "+db);
+app.get('/', exports.index);
+app.get('/api/stocks/:id?', exports.stocks);
 };
